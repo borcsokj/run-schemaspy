75bae799cc49bb87d3c43d7da926813761120447
Börcsök József <borcsok.jozsef@alerant.hu>
2025-06-10 09:38:01 +0200

    add double quotes

--- docker/schemaspy.sh
+++ docker/schemaspy.sh
@@ -11,4 +11,4 @@ fi
 echo "Using drivers:"
 find $DRIVER_PATH -maxdepth 1 -type f -name "*.jar" | sed -e 's/  */, /g'
 
-exec java -jar /usr/local/lib/schemaspy/schemaspy.jar -dp $DRIVER_PATH "$@"
+exec java -jar /usr/local/lib/schemaspy/schemaspy.jar -dp "${DRIVER_PATH}" "$@"
